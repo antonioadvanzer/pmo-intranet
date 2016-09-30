1a5b94d61b20714189b2a2b1cd7cdf6ba66c4edb
José Antonio Báez Alfaro <antoniobaez@MacBook-Pro-de-Antonio.local>
2016-09-30 10:15:27 -0500

    repository created

---
+++ database/migrations/2016_09_29_220828_create_empresa_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Create table to save companies information
+        Schema::create('company', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name',30);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropifExists('company');
+    }
+}
