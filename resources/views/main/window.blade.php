a424972a439dd5ef555d8c73e348cab53f39995d
antonioadvanzer <antonio.baez@advanzer.com>
2016-12-07 17:50:34 -0600

    window and viewer completed

--- resources/views/main/window.blade.php
+++ resources/views/main/window.blade.php
@@ -6,10 +6,33 @@
     <div class="container" align="center">
         <h1 class="titleMenu">Archivos</h1>
     </div>
+    
+    <div class="container">
+        <nav id="menu">
+            <h1 id="sitename">Route</h1>
+            <ul id="foro">
+            <li><a href="#">Home</a><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></li>
+            </ul>
+        </nav>
+    </div>
+
     <div id="desktop" class="container" >
-        <br><br>
+        
         <input id="folder" type="hidden" value="{{ $link }}">
-        <div id="cw" class="window" style="background-color:white;">
+        
+        <div class="window" style="background-color:white;">
+            <div class="barra">
+                <button id="before" type="button" class="boton" aria-label="Left Align">
+                  <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+                </button>
+                <button id="after" type="button" class="boton" aria-label="Left Align">
+                  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                </button>
+                <button id="close" type="button" class="boton" aria-label="Left Align">
+                  <span class="glyphicon glyphicon glyphicon-remove" aria-hidden="true"></span>
+                </button>
+                Carpeta o archivo
+            </div>
             <!--<div class="ico folder">
                 <br><br><br><br><br><a href="#">Arquitectura e instalaci&oacute;n</a>
             </div>
@@ -49,7 +72,8 @@
             <div class="icon mpp">MPP
                 <br><br><br><br><a href="#">Plan de proyecto final (real)</a>
             </div>-->
-            
+            <div id="cw"></div>
+            <div id="ff"></div>
         </div>
         
         <br><br><br><br><br><br><br><br>
@@ -62,32 +86,107 @@
         $(document).ready(function(){
             
             getResource($("#folder").val());
+            addDir($("#folder").val());
+            updatePath();
             //getResource("ss");
             
-            this.current = $("#folder").val();
+            //this.current = $("#folder").val();
             
             
-            /*$("div.element-dir").bind("click", function(e) {
-                //route = $(this).find(".dir").value();
-                //alert(route);
-                alert("aaa");
-            });*/
+            $('#before').hide();
+            $('#after').hide();
+            $('#close').hide();
+            $('#ff').hide();
             
+            $('#before').click(function(){
+                 //getResource(before);
+                getBack();
+                updatePath();
+            });
+            $('#after').click(function(){
+                 //getResource(after);
+                getNext();
+                updatePath();
+            });
         });
         
-        var before = "";
-        var current = "";
-        var after = "";
-        var window = "";
+        var pages = [];
+        var pointer = -1;
+        //var before = "";
+        //var current = "";
+        //var after = "";
+        //var folder = "";
         
         function getViewer(file){
             return "<iframe src='http://docs.google.com/gview?url="+file+"&embedded=true' width ='100%' height='600'></iframe>";
         }
         
+        function closeViewer(){
+            //console.log(folder);
+            //$("#cw").html(folder);
+            $('#cw').show();
+            $('#before').show();
+            $('#close').hide();
+            $('#ff').hide();
+        }
+        
+        function updatePath(){
+            //alert(pages[pointer]);
+            element = pages[pointer];
+            elements = element.split("/");
+            
+            path = "";
+            
+            for(i = 1; i < elements.length; i++){
+               path += '<li><a href="#">'+elements[i]+'</a><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></li>'; 
+            }
+            //alert(path);
+            $("#foro").html(path);
+        }
+        
+        function addDir(dir){
+            
+            if((pages.length-1) > pointer){
+                pages.length = pointer+1;
+            }
+            
+            pointer++;
+            pages.push(dir);
+            
+            //this.current = this.pages.pop();
+            //alert(this.pointer+" "+this.current);
+            //alert(pointer+" "+pages[pointer-1]+" "+pages.length);
+            
+            console.log(pages);
+            
+            if(pages[pointer-1] != null){
+                $('#before').show();
+            }
+            $('#after').hide();
+        }
+        
+        function getBack(){
+            pointer--;
+            getResource(pages[pointer]);
+            if(pages[pointer-1] == null){
+                $('#before').hide();
+            }
+            $('#after').show();
+        }
+        
+        function getNext(){
+            pointer++;
+            getResource(pages[pointer]);
+            if(pages[pointer+1] == null){
+                $('#after').hide();
+            }
+            $('#before').show();
+        }
+        
         function getResource(dir){
             
-            this.before = current;
-            this.current = dir;
+            //this.updatePosition();
+            $("#cw").hide();
             
             $.ajaxSetup({
                 headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
@@ -156,16 +255,18 @@
                                 +'<a href="#">'+data[i]["name"]+'</a>'
                                 +'</div>';
                     
+                    
                     $("#cw").append(value);
                     
                     $("#element-dir"+i).click(function(e) {
                         route = $(this).find(".dir").val();
-                        //alert(route);
-                        getResource(route);
-                        //getResource("ss");
                         
-                        before = this.current;
-                        current = route;
+                        getResource(route);
+                        addDir(route);
+                        updatePath();
+                        
+                        //this.before = this.current;
+                        //this.current = route;
                     });
                         
                     $("#element-file"+i).click(function(e) {
@@ -173,16 +274,29 @@
                         
                         viewer = getViewer(route);
                         
-                        window = $("#cw").html();
+                        //window = $("#cw").html();
+                        //folder = $("#cw").html();
+                        $("#cw").hide();
+                        $('#before').hide();
+                        $('#after').hide();
                         
-                        $("#cw").html(viewer);
+                        $("#ff").html(viewer);
+                        $("#ff").show();
+                        
+                        $('#close').click(function(){
+                            closeViewer();
+                        });
+                        
+                        $('#close').show();
                        
                     });
                 }
-                
+
                 }else{
-                    $("#cw").append("<div align='center' class='alert alert-info'>Esta carpeta esta vacia</div><br><br><br><br><br><br>");
+                    $("#cw").append("<div align='center' class='alert alert-warning'>Esta carpeta esta vacia</div><br><br><br><br><br><br>");
                 }
+                
+                $("#cw").show();
                 
             }).fail(function(data) {
                 console.log(data);
